bd09e92d955193d6b1df72199847d01dcb231a56
Danny <mtme13@hotmail.com>
2020-01-21 16:29:33 +0000

    Edit book detail component to use Observable for retrieving id

--- src/app/book-detail/book-detail.component.ts
+++ src/app/book-detail/book-detail.component.ts
@@ -3,6 +3,7 @@ import IBook from "../books/book";
 import { ActivatedRoute } from "@angular/router";
 import { BookService } from "../books/book.service";
 import { Location } from "@angular/common";
+import { tap, switchMap, map } from "rxjs/operators";
 @Component({
   selector: "app-book-detail",
   templateUrl: "./book-detail.component.html",
@@ -23,10 +24,26 @@ export class BookDetailComponent implements OnInit {
     // this.bookService.getBook(id).subscribe(book => (this.book = book));
 
     // USING OBSERVABLE
-    this.route.paramMap.subscribe(params => {
-      let id = +params.get("id");
-      this.bookService.getBook(id).subscribe(book => (this.book = book));
-    });
+    this.route.paramMap
+      .pipe(
+        tap(console.log),
+        switchMap(params => this.bookService.getBook(+params.get("id")))
+      )
+      .subscribe(book => {
+        this.book = book;
+        this.pageTitle = `${book.title} Details`;
+      });
+
+    // switchMap(params => {
+    //   let id = params.get("id");
+    //   console.log(id);
+    //   return this.bookService.getBook(id);
+    // })
+
+    // this.route.paramMap.subscribe(params => {
+    //   let id = +params.get("id");
+    //   this.bookService.getBook(id).subscribe(book => (this.book = book));
+    // });
   }
   // take user to previous page
   goBack() {
